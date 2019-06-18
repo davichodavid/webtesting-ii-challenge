f628aee60d4d580ed482e72f23d3d0e9bfb87cb8
davichodavid <davichodavid@yahoo.com>
2019-06-18 16:32:02 -0400

    completed the display and dash functionality

--- play-ball/src/App.js
+++ play-ball/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 
-import Display from "./Components/Display";
 import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <Display />
       <Dashboard />
     </div>
   );
--- play-ball/src/Components/Dashboard.js
+++ play-ball/src/Components/Dashboard.js
@@ -10,36 +10,51 @@ class Dashboard extends React.Component {
 
   addStrike = () => {
     this.setState({ ...this.state, strike: this.state.strike + 1 });
+    if (this.state.strike >= 2) {
+      this.setState({ ...this.state, strike: 0, ball: 0 });
+    }
   };
 
   addBall = () => {
-    this.setState({ ...this.state, ball: this.state.strike + 1 });
+    this.setState({ ...this.state, ball: this.state.ball + 1 });
+    if (this.state.ball >= 3) {
+      this.setState({ ...this.state, strike: 0, ball: 0 });
+    }
   };
 
   addFoul = () => {
-    this.setState({ ...this.state, foul: this.state.strike + 1 });
+    if (this.state.strike <= 1) {
+      this.setState({ ...this.state, strike: this.state.strike + 1 });
+    }
   };
 
   addHit = () => {
-    this.setState({ ...this.state, hit: this.state.strike + 1 });
+    this.setState({ ...this.state, strike: 0, ball: 0 });
   };
 
   render() {
+    const { ball, strike } = this.state;
     return (
-      <div>
-        <button name="strike" onClick={this.addStrike}>
-          strike
-        </button>
-        <button name="ball" onClick={this.addBall}>
-          ball
-        </button>
-        <button name="foul" onClick={this.addFoul}>
-          foul
-        </button>
-        <button name="hit" onClick={this.addHit}>
-          hit
-        </button>
-      </div>
+      <>
+        <div className="display">
+          <h2>balls {ball}</h2>
+          <h2>strikes {strike}</h2>
+        </div>
+        <div>
+          <button name="strike" onClick={this.addStrike}>
+            strike
+          </button>
+          <button name="ball" onClick={this.addBall}>
+            ball
+          </button>
+          <button name="foul" onClick={this.addFoul}>
+            foul
+          </button>
+          <button name="hit" onClick={this.addHit}>
+            hit
+          </button>
+        </div>
+      </>
     );
   }
 }
