bf83375908c35239ab062256f859e19b6ada7951
davichodavid <davichodavid@yahoo.com>
2019-06-18 17:49:15 -0400

    MVP with minimal test

--- play-ball/src/App.test.js
+++ play-ball/src/App.test.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("<App /> ", () => {
+  it("renders the app component", () => {
+    render(<App />);
+  });
 });
